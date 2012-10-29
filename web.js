d349bbad4be23d63b26b7185310744b8872814b9
Kimmo Puputti <kpuputti@gmail.com>
2012-10-29 18:28:27 +0200

    Serve static files under 'public' directory.

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 5000;
+app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function (request, response) {
-    response.send('hello, world');
+    response.sendfile('public/index.html');
 });
+
+var port = process.env.PORT || 5000;
 
 app.listen(port, function () {
     console.log('Listening on port: ' + port);
