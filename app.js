a3bb8ba57067bf60f48527d4d41d6ce69c12b0bf
Kimmo Puputti <kpuputti@gmail.com>
2012-11-13 17:24:53 +0200

    Setup whole app from scratch.

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var consolidate = require('consolidate');
+var swig = require('swig');
+
+var app = express();
+var APP_ENV = app.get('env');
+var IS_PRODUCTION = APP_ENV === 'production';
+
+console.log('create app in env: ' + APP_ENV);
+
+app.configure(function () {
+
+    // Swig templating config
+
+    app.engine('.html', consolidate.swig);
+    app.set('view engine', 'html');
+    app.set('views', __dirname + '/views');
+    app.set('view cache', IS_PRODUCTION);
+    swig.init({
+        root: __dirname + '/views',
+        allowErrors: true,
+        cache: IS_PRODUCTION
+    });
+
+    // Other Express config
+
+    app.set('case sensitive routing', true);
+    app.set('strict routing', true);
+
+    // Middleware
+
+    app.use(express.logger(IS_PRODUCTION ? 'default' : 'dev'));
+    app.use(express.compress());
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.cookieParser(process.env.COOKIE_SECRET || 'cookie secret'));
+    app.use(express.session({
+        secret: process.env.SESSION_SECRET || 'session secret'
+    }));
+    app.use(express.csrf());
+
+    if (!IS_PRODUCTION) {
+        app.use(express.errorHandler());
+    }
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler());
+});
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+module.exports = app;
