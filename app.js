e0aac821ffb242e3d45fdb5f7451ef3925041d18
Kimmo Puputti <kpuputti@gmail.com>
2012-11-14 12:35:08 +0200

    Organize config and change to use cookie sessions.

--- app.js
+++ app.js
@@ -4,23 +4,30 @@ var swig = require('swig');
 var path = require('path');
 
 var app = express();
-var APP_ENV = app.get('env');
-var IS_PRODUCTION = APP_ENV === 'production';
-
-console.log('create app in env: ' + APP_ENV);
 
 app.configure(function () {
+
+    var APP_ENV = app.get('env');
+    var IS_PRODUCTION = APP_ENV === 'production';
+    var USE_VIEW_CACHE = IS_PRODUCTION;
+    var PATH_TO_VIEWS = path.join(__dirname, '/views');
+    var LOG_MODE = IS_PRODUCTION ? 'default' : 'dev';
+    var COOKIE_SECRET = process.env.COOKIE_SECRET || 'cookie secret';
+    var SESSION_SECRET = process.env.SESSION_SECRET || 'session secret';
+    var PATH_TO_PUBLIC = path.join(__dirname, 'public');
+
+    console.log('configure app in env: ' + APP_ENV);
 
     // Swig templating config
 
     app.engine('.html', consolidate.swig);
     app.set('view engine', 'html');
-    app.set('views', __dirname + '/views');
-    app.set('view cache', IS_PRODUCTION);
+    app.set('views', PATH_TO_VIEWS);
+    app.set('view cache', USE_VIEW_CACHE);
     swig.init({
-        root: __dirname + '/views',
+        root: PATH_TO_VIEWS,
         allowErrors: true,
-        cache: IS_PRODUCTION
+        cache: USE_VIEW_CACHE
     });
 
     // Other Express config
@@ -30,25 +37,28 @@ app.configure(function () {
 
     // Middleware
 
-    app.use(express.logger(IS_PRODUCTION ? 'default' : 'dev'));
+    app.use(express.logger(LOG_MODE));
     app.use(express.compress());
     app.use(express.favicon());
     app.use(express.bodyParser());
-    app.use(express.cookieParser(process.env.COOKIE_SECRET || 'cookie secret'));
-    app.use(express.session({
-        secret: process.env.SESSION_SECRET || 'session secret'
+    app.use(express.cookieParser(COOKIE_SECRET));
+    app.use(express.cookieSession({
+        secret: SESSION_SECRET
     }));
     app.use(express.csrf());
 
     var dayInMilliseconds = 24 * 60 * 60 * 1000;
-    app.use(express.static(path.join(__dirname, 'public'), {
+    app.use(express.static(PATH_TO_PUBLIC, {
         maxAge: dayInMilliseconds
     }));
 
+    // Add extra error reporting for non-production envs
     if (!IS_PRODUCTION) {
         app.use(express.errorHandler());
     }
 });
+
+// Routing
 
 app.get('/', function (req, res) {
     res.render('index');
