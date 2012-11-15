0472ff3b9c2af3c9fbd962b3d3a14a0d584b1193
Kimmo Puputti <kpuputti@gmail.com>
2012-11-15 12:26:11 +0200

    Compile sass files also when images are changed.

--- grunt.js
+++ grunt.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
                 tasks: 'lint'
             },
             styles: {
-                files: ['public/sass/**/*.scss'],
+                files: ['public/sass/**/*.scss', 'public/img/*'],
                 tasks: 'compass:dev'
             }
         },
