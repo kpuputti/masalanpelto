a195c1d0db9e2d8f6acc730e5dd9e4cf3bd799a5
Kimmo Puputti <kpuputti@gmail.com>
2012-12-11 12:36:36 +0200

    Add test helpers.

---
+++ test/helpers.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var Q = require('q');
+var expect = require('chai').expect;
+
+exports.loginAs = function (username, browser) {
+    var deferred = Q.defer();
+
+    var password = process.env['TEST_PASSWORD_' + username.toUpperCase()];
+    if (!password) {
+        return deferred.reject(new Error('Test password not found for user: ' + username));
+    }
+
+    browser
+        .visit('/kirjaudu-sisaan')
+        .then(function () {
+            return browser
+                .fill('username', username)
+                .fill('password', password)
+                .pressButton('[type=submit]');
+        })
+        .then(function () {
+            expect(browser.location.pathname).to.equal('/');
+            expect(browser.text('.messages')).to.equal('Kirjautunut sisään tunnuksella ' + username);
+            expect(browser.text('.user-actions')).to.equal(username + ' | Kirjaudu ulos');
+            deferred.resolve();
+        })
+        .fail(deferred.reject);
+
+    return deferred.promise;
+};
