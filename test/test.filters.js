1432fd822964f43dcbd619f1af0ba3eb605ab62b
Kimmo Puputti <kpuputti@gmail.com>
2012-12-11 12:37:23 +0200

    Use chai.expect instead of chai.should in BDD tests.

--- test/test.filters.js
+++ test/test.filters.js
@@ -2,42 +2,40 @@
 
 'use strict';
 
-var chai = require('chai');
-chai.should();
-
+var expect = require('chai').expect;
 var filters = require('../lib/filters');
 
 describe('Custom Swig template filters', function () {
 
     it('should handle empty size', function () {
-        filters.filesize('0').should.equal('0 B');
+        expect(filters.filesize('0')).to.equal('0 B');
     });
 
     it('should handle exact sizes for byte, kilobyte, megabyte and gigabyte', function () {
-        filters.filesize('1').should.equal('1 B');
-        filters.filesize('1024').should.equal('1 kB');
-        filters.filesize('1048576').should.equal('1 MB'); // Math.pow(1024, 2)
-        filters.filesize('1073741824').should.equal('1 GB'); // Math.pow(1024, 3)
+        expect(filters.filesize('1')).to.equal('1 B');
+        expect(filters.filesize('1024')).to.equal('1 kB');
+        expect(filters.filesize('1048576')).to.equal('1 MB'); // Math.pow(1024, 2)
+        expect(filters.filesize('1073741824')).to.equal('1 GB'); // Math.pow(1024, 3)
     });
 
     it('should handle random whole number sizes', function () {
-        filters.filesize('1000').should.equal('1000 B');
-        filters.filesize('500').should.equal('500 B');
-        filters.filesize('512000').should.equal('500 kB');
-        filters.filesize('524288000').should.equal('500 MB');
-        filters.filesize('536870912000').should.equal('500 GB');
+        expect(filters.filesize('1000')).to.equal('1000 B');
+        expect(filters.filesize('500')).to.equal('500 B');
+        expect(filters.filesize('512000')).to.equal('500 kB');
+        expect(filters.filesize('524288000')).to.equal('500 MB');
+        expect(filters.filesize('536870912000')).to.equal('500 GB');
     });
 
     it('should handle decimal points', function () {
-        filters.filesize('1034').should.equal('1.01 kB');
-        filters.filesize('1900').should.equal('1.86 kB');
-        filters.filesize('2012').should.equal('1.96 kB');
-        filters.filesize('1000000').should.equal('976.56 kB');
+        expect(filters.filesize('1034')).to.equal('1.01 kB');
+        expect(filters.filesize('1900')).to.equal('1.86 kB');
+        expect(filters.filesize('2012')).to.equal('1.96 kB');
+        expect(filters.filesize('1000000')).to.equal('976.56 kB');
     });
 
     it('should use gigabytes when size is tera or larger', function () {
         var tera = Math.pow(1024, 4);
-        filters.filesize(tera.toString()).should.equal('1024 GB');
+        expect(filters.filesize(tera.toString())).to.equal('1024 GB');
     });
 
 });
