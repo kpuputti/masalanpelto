60a0b4606794e14a6e27b732a1ddfc6061cbcec3
Kimmo Puputti <kimmo.puputti@futurice.com>
2013-10-15 22:50:32 +0300

    Rewrite grunt config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,21 @@
-/*global module:false*/
 module.exports = function (grunt) {
 
     'use strict';
 
     grunt.initConfig({
-        pkg: '<json:package.json>',
-        meta: {
-            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-                '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-                '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-                '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-                ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-        },
-        lint: {
-            files: ['grunt.js', 'src/js/**/*.js', 'lib/**/*.js', 'test/**/*.js']
-        },
-        concat: {
-            dist: {
-                src: ['<banner:meta.banner>', '<file_strip_banner:src/js/app.js>'],
-                dest: 'public/dist/app.js'
-            }
-        },
-        min: {
-            dist: {
-                src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-                dest: 'public/dist/app.min.js'
-            }
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            allFiles: ['Gruntfile.js', 'lib/**/*.js', 'src/**/*.js', 'test/**/*.js']
         },
         compass: {
-            dev: {
+            dist: {
                 options: {
                     sassDir: 'src/sass',
                     cssDir: 'public/dist',
-                    imagesDir: 'public/img',
+                    environment: 'production',
                     outputStyle: 'compressed', // One of: nested, expanded, compact, compressed
                     force: true
                 }
@@ -40,51 +23,19 @@ module.exports = function (grunt) {
         },
         watch: {
             scripts: {
-                files: '<config:lint.files>',
-                tasks: 'lint'
+                files: ['<%= jshint.allFiles %>'],
+                tasks: ['jshint']
             },
             styles: {
-                files: ['grunt.js', 'src/sass/**/*.scss', 'public/img/*'],
-                tasks: 'compass:dev'
+                files: ['src/sass/**/*.scss'],
+                tasks: ['compass']
             }
-        },
-        jshint: {
-            options: {
-                // enforcing options
-
-                camelcase: true,
-                curly: true,
-                eqeqeq: true,
-                forin: true,
-                immed: true,
-                indent: 4,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                nonew: true,
-                quotmark: 'single',
-                undef: true,
-                unused: true,
-                strict: true,
-                trailing: true,
-
-                // environment
-
-                devel: true,
-                node: true
-
-            },
-            globals: {}
-        },
-        uglify: {}
+        }
     });
 
-    // grunt.loadNpmTasks('grunt-cont rib-jshint');
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Default task.
-    //grunt.registerTask('default', 'lint concat min compass:dev');
-    grunt.registerTask('default', 'compass:dev');
-
+    grunt.registerTask('default', ['jshint', 'compass']);
 };
